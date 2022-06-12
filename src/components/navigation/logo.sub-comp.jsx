4dd22a8064613f5928d5e0deac73518605176cd5
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-12 14:27:24 +0200

    removed obsolete files

--- src/components/navigation/logo.sub-comp.jsx
+++
@@ -1,8 +0,0 @@
-import { Image } from "@chakra-ui/react";
-import LogoImg from "../../assets/logo.png";
-
-const Logo = () => {
-  return <Image src={LogoImg} boxSize="100%" />;
-};
-
-export default Logo;
