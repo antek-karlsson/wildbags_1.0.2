207a7b3e7b956ca846b9b7760d5f36b587ec0bff
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-12 14:14:58 +0200

    created Homepage component

--- src/routes/Home.page.jsx
+++ src/routes/Home.page.jsx
@@ -1,11 +1,19 @@
 import { Container } from "@chakra-ui/react";
+import Footer from "../components/footer/footer.component";
 import Navigation from "../components/navigation/navigation.comp";
+import Hero from "../components/hero/hero.comp";
+import Infobox1 from "../components/infobox1/infobox1.comp";
+import Infobox2 from "../components/infobox2/infobox2.comp";
 
 const Home = () => {
   return (
-    <Container maxW="90vw">
+    <>
       <Navigation />
-    </Container>
+      <Hero />
+      <Infobox1 />
+      <Infobox2 />
+      <Footer />
+    </>
   );
 };
 
