1a9b733041275b0dde33886c6c92756c7e6d6402
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-12 14:27:04 +0200

    created homepage info section

---
+++ src/components/infobox1/infobox1.comp.jsx
@@ -0,0 +1,33 @@
+import { Stack, Flex, Heading, Text, Image } from "@chakra-ui/react";
+import infobox_1 from "../../assets/images/home_images/infobox_1.png";
+
+const Infobox1 = () => {
+  return (
+    <Stack p={8} direction={{ base: "column", md: "row" }}>
+      <Flex flex={1}>
+        <Image alt={"infobox_1"} objectFit={"cover"} src={infobox_1} />
+      </Flex>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
+            <Text>Info</Text>
+          </Heading>
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"gray.500"}>
+            Wszystkie produkty , które tu zobaczysz stworzone są zgodnie z ideą
+            #upcykling , czyli zmniejszeniem zużycia surowców naturalnych oraz
+            wytwarzanych odpadów poprzez tworzenie produktów z materiałów z
+            drugiej ręki o wyższej wartości niż pojedyncze tworzywo użyte do
+            stworzenia danej rzeczy.
+          </Text>
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"gray.500"}>
+            To rędzieło, które powstaje z ciągłych poszukiwań oryginalnych
+            materiałów z drugiego obiegu i jest kreatywnie przekształcane w
+            zupełnie nową rzecz.
+          </Text>
+        </Stack>
+      </Flex>
+    </Stack>
+  );
+};
+
+export default Infobox1;
---
+++ src/components/infobox2/infobox2.comp.jsx
@@ -0,0 +1,30 @@
+import { Stack, Flex, Heading, Text, Image } from "@chakra-ui/react";
+import infobox_2 from "../../assets/images/home_images/infobox_2.png";
+
+const Infobox2 = () => {
+  return (
+    <Stack bg={"teal.500"} p={8} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}>
+            <Text>
+              Chciałabyś mieć wyjątkową torbę lub kosmetyczkę z wymarzonego
+              materiału w ulubionym kolorze?
+            </Text>
+          </Heading>
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"white"}>
+            Napisz do mnie - dogramy wspólnie szczegóły :)
+          </Text>
+          <Text fontSize={{ base: "md", lg: "lg" }} color={"white"}>
+            wildbags@wildbags.pl
+          </Text>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image alt={"infobox_1"} objectFit={"cover"} src={infobox_2} />
+      </Flex>
+    </Stack>
+  );
+};
+
+export default Infobox2;
