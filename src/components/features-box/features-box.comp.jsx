34162cc7efe58e1d02d1269781b23b882c9760b7
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-14 19:27:42 +0200

    created FeaturesBox component for home page

---
+++ src/components/features-box/features-box.comp.jsx
@@ -0,0 +1,20 @@
+import { SimpleGrid } from "@chakra-ui/react";
+import features_1 from "../../assets/images/features/features_1.jpg";
+import features_2 from "../../assets/images/features/features_2.jpg";
+import features_3 from "../../assets/images/features/features_3.jpg";
+import features_4 from "../../assets/images/features/features_4.jpg";
+
+import FeatureCard from "./feature-card.sub-comp";
+
+const FeaturesBox = () => {
+  return (
+    <SimpleGrid columns={[1, null, 2, 4]} spacing={8} p={8}>
+      <FeatureCard src={features_1} label={"JESTEŚMY ECO"} />
+      <FeatureCard src={features_2} label={"WYŻSZA FORMA UPCYKLINGU"} />
+      <FeatureCard src={features_3} label={"SLOW FASHION"} />
+      <FeatureCard src={features_4} label={"ZERO WASTE"} />
+    </SimpleGrid>
+  );
+};
+
+export default FeaturesBox;
