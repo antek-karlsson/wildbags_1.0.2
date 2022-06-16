80d6579b6bb1eb80bd92ef91adb4849a76656b92
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-16 13:21:31 +0200

    changed icons

--- src/components/navigation/color-mode-btn.sub-comp.jsx
+++ src/components/navigation/color-mode-btn.sub-comp.jsx
@@ -1,5 +1,5 @@
 import { IconButton, useColorMode } from "@chakra-ui/react";
-import { MdOutlineWbSunny, MdOutlineNightlight } from "react-icons/md";
+import { FiSun, FiMoon } from "react-icons/fi";
 
 const ColorModeBtn = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -7,9 +7,7 @@ const ColorModeBtn = () => {
     <IconButton
       alignSelf={"flex-end"}
       colorScheme={"teal"}
-      icon={
-        colorMode === "light" ? <MdOutlineNightlight /> : <MdOutlineWbSunny />
-      }
+      icon={colorMode === "light" ? <FiMoon /> : <FiSun />}
       isRound="true"
       onClick={toggleColorMode}
       fontSize={"1.5rem"}
