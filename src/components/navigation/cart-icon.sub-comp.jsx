5200da82f0145bc6f07210f43fee4a7877add489
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-16 13:21:02 +0200

    changed icon

--- src/components/navigation/cart-icon.sub-comp.jsx
+++ src/components/navigation/cart-icon.sub-comp.jsx
@@ -1,11 +1,11 @@
 import { IconButton } from "@chakra-ui/react";
-import { FaShoppingBasket } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
 
 const CartIcon = () => {
   return (
     <IconButton
       colorScheme={"teal"}
-      icon={<FaShoppingBasket />}
+      icon={<FiShoppingCart />}
       // bg={"transparent"}
       fontSize={"1.5rem"}
     />
