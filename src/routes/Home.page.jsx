03ebd80333b15e160180c7c8060ad225089bb797
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-17 10:49:01 +0200

    Added contactbox component to home page

--- src/routes/Home.page.jsx
+++ src/routes/Home.page.jsx
@@ -5,6 +5,7 @@ import Hero from "../components/hero/hero.comp";
 import Infobox1 from "../components/infobox1/infobox1.comp";
 import Infobox2 from "../components/infobox2/infobox2.comp";
 import FeaturesBox from "../components/features-box/features-box.comp";
+import Contactbox from "../components/contactbox/contact-box.comp";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Infobox1 />
       <Infobox2 />
       <FeaturesBox />
+      <Contactbox />
       <Footer />
     </>
   );
