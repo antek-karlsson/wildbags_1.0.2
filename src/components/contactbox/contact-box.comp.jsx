27e8db31589f88fd11c0ed1aa17a995616a0aa0c
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-17 19:14:55 +0200

    built out contact form, updated layout, added emailJS functionality

--- src/components/contactbox/contact-box.comp.jsx
+++ src/components/contactbox/contact-box.comp.jsx
@@ -15,43 +15,81 @@ import {
   Input,
   VStack,
   Textarea,
+  Button,
 } from "@chakra-ui/react";
 
 import { FiUser, FiMail } from "react-icons/fi";
+import { RiQuestionMark } from "react-icons/ri";
 
 import contactbox from "../../assets/images/home_images/contactbox.jpg";
 
 const Contactbox = () => {
   const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_uyl801f",
+        "template_idkarqs",
+        form.current,
+        "T1pppy2gD3XZ8hodV"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <Stack bg={"teal.500"} p={8} direction={{ base: "column", md: "row" }}>
-      <Flex p={8} flex={1} align={"center"} justify={"center"}>
-        <VStack>
+    <Stack bg={"teal.500"} p={16} direction={{ base: "column", md: "row" }}>
+      <Flex p={4} flex={1} align={"center"} justify={"center"}>
+        <VStack
+          w={["100%", "100%", "80%"]}
+          as={"form"}
+          ref={form}
+          onSubmit={sendEmail}
+          spacing={8}
+        >
+          <Heading>Kontaktuj się!</Heading>
           <FormControl isRequired>
             <FormLabel>Imię</FormLabel>
             <InputGroup>
               <InputLeftElement children={<FiUser />} />
-              <Input />
+              <Input type={"text"} id={"name"} name={"name"} />
             </InputGroup>
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Email</FormLabel>
             <InputGroup>
               <InputLeftElement children={<FiMail />} />
-              <Input />
+              <Input type={"email"} id={"email"} name={"email"} />
             </InputGroup>
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Temat</FormLabel>
             <InputGroup>
-              <InputLeftElement children={<FiUser />} />
-              <Input />
+              <InputLeftElement children={<RiQuestionMark />} />
+              <Input type={"text"} id={"subject"} name={"subject"} />
             </InputGroup>
           </FormControl>
           <FormControl isRequired>
             <FormLabel>Wiadomość</FormLabel>
-            <Textarea />
+            <Textarea id={"message"} name={"message"} />
           </FormControl>
+          <Button
+            value={"send"}
+            w={"100%"}
+            size={"lg"}
+            type={"submit"}
+            colorScheme={"pink"}
+          >
+            Wyślij
+          </Button>
         </VStack>
       </Flex>
       <Flex flex={1}>
