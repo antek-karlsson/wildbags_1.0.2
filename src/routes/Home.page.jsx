07bea2e6a77ff14cafedef6eb71daf3fa0593750
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-18 14:48:35 +0200

    added carousel component to home page

--- src/routes/Home.page.jsx
+++ src/routes/Home.page.jsx
@@ -1,4 +1,3 @@
-import { Container } from "@chakra-ui/react";
 import Footer from "../components/footer/footer.component";
 import Navigation from "../components/navigation/navigation.comp";
 import Hero from "../components/hero/hero.comp";
@@ -6,12 +5,14 @@ import Infobox1 from "../components/infobox1/infobox1.comp";
 import Infobox2 from "../components/infobox2/infobox2.comp";
 import FeaturesBox from "../components/features-box/features-box.comp";
 import Contactbox from "../components/contactbox/contact-box.comp";
+import Carousel from "../components/carousel/carousel.comp";
 
 const Home = () => {
   return (
     <>
       <Navigation />
       <Hero />
+      <Carousel />
       <Infobox1 />
       <Infobox2 />
       <FeaturesBox />
