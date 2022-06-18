e88ecb115797724d6387367dc97d000191073bb1
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-18 14:45:07 +0200

    removed unneccessary imports, added about page route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
-import { Flex, Heading } from "@chakra-ui/react";
-
 import Home from "./routes/Home.page";
+import About from "./routes/About.page";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 };
