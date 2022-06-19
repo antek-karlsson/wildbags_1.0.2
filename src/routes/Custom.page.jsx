7385a12619470bdf07542f4bf0e74237ee57ee78
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-19 13:50:12 +0200

    created preliminary custom page component

---
+++ src/routes/Custom.page.jsx
@@ -0,0 +1,57 @@
+import { Heading, Text, Image, VStack } from "@chakra-ui/react";
+
+import Navigation from "../components/navigation/navigation.comp";
+import Footer from "../components/footer/footer.component";
+
+import customImg from "../assets/images/custom_images/custom_1.jpg";
+
+const Custom = () => {
+  return (
+    <>
+      <Navigation />
+      <VStack justify={"space-between"} align={"center"}>
+        <Heading size={"xl"} px={{ base: 12, md: 32, lg: 64 }} py={8}>
+          Haute couture, czyli torby i kosmetyczki szyte na zamówienie -
+          specjalnie dla Ciebie :)
+        </Heading>
+
+        <Text
+          mt={24}
+          px={{ base: 12, md: 24, lg: 64 }}
+          pt={12}
+          pb={8}
+          fontSize={{ base: "md", lg: "lg" }}
+        >
+          1. Powiedz mi jakie kolory najbardziej lubisz i jaki materiał do
+          Ciebie przemawia? Czy jesteś fanką stylu retro czy raczej wolisz
+          nowoczesne wzory? Określ mi swoją wymarzoną torbę lub kosmetyczkę w
+          jak najdrobniejszych szczegółach
+        </Text>
+        <Text
+          px={{ base: 12, md: 24, lg: 64 }}
+          py={8}
+          fontSize={{ base: "md", lg: "lg" }}
+        >
+          2. Po ustaleniu wszelkich detali otrzymasz ode mnie zdjęcie materiałów
+          jakie udało mi się dla Ciebie znaleźć ( a może masz jakiś kawałek
+          tkaniny, lub ubrania, który chciałabyś wykorzystać - pisz śmiało -
+          może uda się go wykorzystać )
+        </Text>
+        <Text
+          px={{ base: 12, md: 24, lg: 64 }}
+          pt={8}
+          pb={12}
+          fontSize={{ base: "md", lg: "lg" }}
+          alignSelf={"flex-start"}
+        >
+          3. Czas realizacji zależy od indywidualnych ustaleń i tego jakie
+          materiały mam aktualnie na stanie :)
+        </Text>
+        <Image src={customImg} p={12} borderRadius={"xl"} />
+      </VStack>
+      <Footer />
+    </>
+  );
+};
+
+export default Custom;
