7dc3a91d764ed5155565704d7a2ea96c037be26c
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-19 13:50:39 +0200

    added new route for custom page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/Home.page";
 import About from "./routes/About.page";
+import Custom from "./routes/Custom.page";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/custom" element={<Custom />} />
     </Routes>
   );
 };
