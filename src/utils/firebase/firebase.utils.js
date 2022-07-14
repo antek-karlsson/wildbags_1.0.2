7d661ba0aee0125327db7d63b079d4f9b290ad70
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-14 21:46:28 +0200

    configured & initialized new firebase app

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzUH_s7Dbiy_crH7Zhv29fY4yV6xbJZj0",
+  authDomain: "wildbags-demo.firebaseapp.com",
+  projectId: "wildbags-demo",
+  storageBucket: "wildbags-demo.appspot.com",
+  messagingSenderId: "454333856327",
+  appId: "1:454333856327:web:2d87d78804b2433bf08f62",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
