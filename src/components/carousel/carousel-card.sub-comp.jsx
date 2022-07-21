436c74b68a93496dc9e8052f024e085e61c58fb7
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-21 15:24:52 +0200

    removed border & border radius

--- src/components/carousel/carousel-card.sub-comp.jsx
+++ src/components/carousel/carousel-card.sub-comp.jsx
@@ -1,14 +1,8 @@
-import { VStack, Image, Text, useColorMode } from "@chakra-ui/react";
+import { VStack, Image, Text } from "@chakra-ui/react";
 
 const CarouselCard = ({ imgSrc, prodName, prodPrice }) => {
-  const { colorMode } = useColorMode();
   return (
-    <VStack
-      justify={"center"}
-      align={"center"}
-      border={colorMode === "light" ? "1px solid black" : "1px solid white"}
-      borderRadius={"3xl"}
-    >
+    <VStack justify={"center"} align={"center"}>
       <Image src={imgSrc} p={6} borderRadius={"lg"} />
       <VStack px={6} pb={6}>
         <Text>{prodName}</Text>
