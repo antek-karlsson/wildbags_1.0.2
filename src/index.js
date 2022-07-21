504fdc8ef56ab23a12f8970b4646c8162ee19f41
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-21 15:23:15 +0200

    imported main sass stylesheet

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 import App from "./App";
 
+import "./index.sass";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
