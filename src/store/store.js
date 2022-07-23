5e063632c3a845f6090bbbef13a27c07a10e4e67
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-23 18:30:20 +0200

    set up redux store

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+
+import rootReducer from "./rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [process.env.NODE_ENV !== "production" && logger].filter(Boolean),
+});
+
+export const persistor = persistStore(store);
